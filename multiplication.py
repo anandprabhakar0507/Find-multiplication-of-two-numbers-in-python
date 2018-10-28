59c7c791f75fbc04960188298d67225c7b2917b0
Anand prabhakar <anandprabhakar0507@gmail.com>
2018-10-28 10:05:01 -0700

    Create multiplication.py

---
+++ multiplication.py
@@ -0,0 +1,10 @@
+# This program multiply two numbers
+
+m = 7
+v = 2
+
+# Multiply two numbers
+into= float(m) * float(v)
+
+# Display the Result
+print('The multiplication of {0} and {1} is {2}'.format(m, v, into))
