a803ea6abd00185c2924fd35802f5c72faf1a400
Restyled.io <commits@restyled.io>
2020-10-01 07:31:19 +0000

    Restyled by autopep8

--- multiplication.py
+++ multiplication.py
@@ -4,7 +4,7 @@ m = int(input())
 v = int(input())
 
 # Multiply two numbers
-into= float(m) * float(v)
+into = float(m) * float(v)
 
 # Display the Result
 print('The multiplication of {0} and {1} is {2}'.format(m, v, into))
